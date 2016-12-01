9777748610141e3fab537ba5c3a3b70c3ceb2703
Mc <313412485@qq.com>
2016-12-01 21:18:27 +0800

    P5继承相关代码 P11集合排序Collections.sort方法相关代码

---
+++ src/java/P11/Compare/NovelNews.java
@@ -0,0 +1,31 @@
+package P11.Compare;
+
+/**
+ * Created by Mc on 2016/12/1.
+ */
+public class NovelNews implements Comparable {
+    private int heat;
+
+    public NovelNews(int heat) {
+        this.heat = heat;
+    }
+
+    public int getHeat() {
+        return heat;
+    }
+
+    public void setHeat(int heat) {
+        this.heat = heat;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(heat);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        NovelNews o1 = (NovelNews) o;
+        return heat - o1.heat;
+    }
+}
