239d48c29d1638c790bdc09b66d8d74e872d69a2
Mc <313412485@qq.com>
2016-12-03 08:59:08 +0800

    P5 继承相关代码

---
+++ src/java/P5/Inheritance/Developer.java
@@ -0,0 +1,12 @@
+package P5.Inheritance;
+
+import java.util.Date;
+
+/**
+ * Created by Mc on 2016/12/3.
+ */
+class Developer extends Employee {
+    Developer(String name, double salary, Date hireDay) {
+        super(name, salary, hireDay);
+    }
+}
