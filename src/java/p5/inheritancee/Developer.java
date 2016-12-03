d0e4d914e319dfa691bbd669b5e754f6ee76e723
Mc <313412485@qq.com>
2016-12-03 09:08:15 +0800

    P5 继承相关代码

---
+++ src/java/p5/inheritancee/Developer.java
@@ -0,0 +1,12 @@
+package p5.inheritancee;
+
+import java.util.Date;
+
+/**
+ * Created by Mc on 2016/12/3.
+ */
+class Developer extends Employee {
+    Developer(String name, double salary, Date hireDay) {
+        super(name, salary, hireDay);
+    }
+}
---
+++ src/java/p5/inheritancee/Manager.java
@@ -0,0 +1,25 @@
+package p5.inheritancee;
+
+import java.util.Date;
+
+/**
+ * Created by Mc on 2016/11/29.
+ */
+class Manager extends Employee {
+    private double bonus;
+
+    Manager(String name, double salary, Date hireDay) {
+        super(name, salary, hireDay);
+        bonus = 0;
+    }
+
+    @Override
+    public double getSalary() {
+        double salary = super.getSalary();
+        return salary + bonus;
+    }
+
+    void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+}
