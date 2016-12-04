8a59ae50b0f8df32addcf59d5f18b101ea262699
Mc <313412485@qq.com>
2016-12-04 21:40:14 +0800

    P5 枚举类型相关代码

--- src/java/p5/enumDemo/Size.java
+++ src/java/p5/enumDemo/Size.java
@@ -2,6 +2,7 @@ package p5.enumDemo;
 
 /**
  * Size枚举类
+ * 每个枚举类都隐含继承自Enum类,因此枚举类不能继承其他类，但枚举类能实现接口
  * 每个枚举值都是一个枚举实例，而且不能再被实例化
  * 类似每个枚举实例都是由public static final修饰的常量的语句实例化出来的
  * 因此每个枚举实例都是单例的，枚举也可以拿来写单例对象
@@ -11,7 +12,6 @@ enum Size {
     SMALL("S"),MEDIUM("M"),LARGE("L"),EXTRA_LARGE("XL");
 
     //在定义完枚举值后，还可以自行添加构造器、方法和域
-    //枚举类也能实现接口
     private String abbreviation;
 
     private Size(String abbreviation){
